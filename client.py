b48901e26d11b6572b392d6fc7134cf3c9a3d2ac
Qiaosen Huang <joesonw@gmail.com>
2013-11-15 01:33:55 -0500

    clean up

--- client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-sock.connect(("127.0.0.1",8011));
-
-while(1):
-	msg=raw_input("")
-	sock.send("%8s"%hex(len(msg))[2:])
-	sock.send(msg)
